f61a52e730905d41bae9c971e87fdb011d38e647
Connor Michael Gerrits <connormgerrits@Connors-MacBook-Air.local>
2019-09-30 21:10:11 -0400

    First commit

---
+++ data-types.js
@@ -0,0 +1,17 @@
+var num = 5
+var string = "word"
+var float = 5.5
+var array = ["Connor", "Michael", "Gerrits"]
+var boolean = true
+var object = {
+    name: 'connor',
+    age: 24,
+    weight: 148.5,
+}
+
+console.log(num)
+console.log(string)
+console.log(float)
+console.log(array)
+console.log(boolean)
+console.log(object)
