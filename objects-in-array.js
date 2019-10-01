c18748182ffe293e62ad59e8f71c3d29887e0d66
Connor Michael Gerrits <connormgerrits@Connors-MacBook-Air.local>
2019-09-30 21:42:53 -0400

    added new file with objects in an array

---
+++ objects-in-array.js
@@ -0,0 +1,18 @@
+let cars = [
+    {
+     make: "Toyota", 
+     model: "Highlander", 
+     year: 2004
+    }, 
+    {    
+     make: "Chevy", 
+     model: "Camaro", 
+     year: 2013
+    },
+    {
+     make: "Dodge", 
+     model: "Charger", 
+     year: 2019
+    }];
+
+console.log(cars);
