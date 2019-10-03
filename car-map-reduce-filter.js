cb8cd0b00cebba920e6c1b5ed4e2bb718a46e921
Connor Michael Gerrits <connormgerrits@connors-air.worcester.local>
2019-10-02 21:22:20 -0400

    even more practice with maps

--- car-map-reduce-filter.js
+++ car-map-reduce-filter.js
@@ -20,6 +20,13 @@ var oldCars = cars.filter( (car) => {
     return car.year < 2019
 })
 
+// get an object for each car that includes the existing data as well
+// as a single full string property named 'display'
+// for example '2012 for fusion'
+var displayData = cars.map( (car) => {
+    car.display = car.year + ' ' + car.make + ' ' + car.model
+    return car
+})
 
-
-console.log(oldCars)
+// console.log(oldCars)
+console.log(displayData)
